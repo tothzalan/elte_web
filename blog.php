b28efb763cd7145bd5c7ff1aa8dbfa69d45fbc67
tothzalan <toth.zalan03@gmail.com>
2022-07-28 01:43:51 +0200

    Terrible blog system

--- blog.php
+++ blog.php
@@ -1,4 +1,24 @@
 <?php
+	$filtered = filter_input(INPUT_GET, 'name', FILTER_DEFAULT) ?? '';
+	$post_name = htmlspecialchars($filtered);
+
+	$file = [];
+	if($post_name == '') {
+		if($handle = opendir('posts/')) {
+			while (($file_name = readdir($handle)) !== false) {
+				if(strlen($file_name) > 2)
+					array_push($file, $file_name);
+			}
+			closedir($handle);
+		}
+	} else { 
+		if(file_exists("posts/$post_name")) {
+			$file_contents = file_get_contents("posts/$post_name");
+			$file = explode(PHP_EOL, $file_contents);
+		}	
+		else
+			array_push($file, "Nincs ilyen fájl!");
+	}
 ?>
 <!DOCTYPE html>
 <html lang="hu">
@@ -12,9 +32,21 @@
 <body>
 	<h1>Blog</h1>
 	<p>Itt olvashatóak a blog posztjaim:</p>
+	<?php if($post_name == '') : ?>
 	<ul>
-		<li>Nagy üresség</li>
+		<?php if(count($file) > 0) : ?>
+			<?php foreach($file as $post) : ?>
+				<li><a href="blog?name=<?= $post ?>"><?= $post ?></a></li>
+			<?php endforeach ?>
+		<?php else : ?>
+			<li>Nagy üresség :(</li>
+		<?php endif ?>
 	</ul>
+	<?php else : ?>
+		<?php foreach($file as $line) : ?>
+			<p><?= $line ?></p>
+		<?php endforeach ?>
+	<?php endif ?>
 	<a href="index">vissza</a>
 </body>
 </html>
